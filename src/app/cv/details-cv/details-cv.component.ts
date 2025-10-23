f404ad5706ef64a559e87c0939efc994896d6423
fouziMariem <mariem.elfouzi@insat.ucar.tn>
2025-10-23 22:42:59 +0100

    refactor: update DetailsCvComponent to use async pipe for CV data retrieval

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Cv } from '../model/cv';
 import { CvService } from '../services/cv.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { APP_ROUTES } from '../../../config/routes.config';
 import { AuthService } from '../../auth/services/auth.service';
+import { catchError, map, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-details-cv',
   templateUrl: './details-cv.component.html',
   styleUrls: ['./details-cv.component.css'],
 })
-export class DetailsCvComponent implements OnInit {
-  cv: Cv | null = null;
+export class DetailsCvComponent {
+  cv$ = this.activatedRoute.params.pipe(
+    switchMap((params) =>
+      this.cvService.getCvById(+params['id']).pipe(
+        catchError((error) => {
+          // handle error reactively
+          this.toastr.error(
+            `Erreur: impossible de récupérer le CV. Redirection en cours...`
+          );
+          this.router.navigate([APP_ROUTES.cv]);
+          return of(null);
+        })
+      )
+    )
+  );
+
   constructor(
     private cvService: CvService,
     private router: Router,
@@ -21,17 +36,6 @@ export class DetailsCvComponent implements OnInit {
     public authService: AuthService
   ) {}
 
-  ngOnInit() {
-    const id = this.activatedRoute.snapshot.params['id'];
-    this.cvService.getCvById(+id).subscribe({
-        next: (cv) => {
-          this.cv = cv;
-        },
-        error: (e) => {
-          this.router.navigate([APP_ROUTES.cv]);
-        },
-      });
-  }
   deleteCv(cv: Cv) {
     this.cvService.deleteCvById(cv.id).subscribe({
       next: () => {
@@ -40,7 +44,7 @@ export class DetailsCvComponent implements OnInit {
       },
       error: () => {
         this.toastr.error(
-          `Problème avec le serveur veuillez contacter l'admin`
+          `Problème avec le serveur, veuillez contacter l'admin.`
         );
       },
     });
