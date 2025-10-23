343b53099e188504acf83c40b346eac8c1f09151
fouziMariem <mariem.elfouzi@insat.ucar.tn>
2025-10-23 20:03:20 +0100

    refactor: update CvComponenet to use async pipe for observables

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,16 +1,17 @@
-import { Component } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
-import { ToastrService } from "ngx-toastr";
-import { CvService } from "../services/cv.service";
+import { Component } from '@angular/core';
+import { Cv } from '../model/cv';
+import { LoggerService } from '../../services/logger.service';
+import { ToastrService } from 'ngx-toastr';
+import { CvService } from '../services/cv.service';
+import { catchError, Observable, of } from 'rxjs';
 @Component({
-  selector: "app-cv",
-  templateUrl: "./cv.component.html",
-  styleUrls: ["./cv.component.css"],
+  selector: 'app-cv',
+  templateUrl: './cv.component.html',
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent {
-  cvs: Cv[] = [];
-  selectedCv: Cv | null = null;
+  cvs$: Observable<Cv[]>;
+  selectedCv$: Observable<Cv>;
   /*   selectedCv: Cv | null = null; */
   date = new Date();
 
@@ -19,19 +20,18 @@ export class CvComponent {
     private toastr: ToastrService,
     private cvService: CvService
   ) {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
+    this.cvs$ = this.cvService.getCvs().pipe(
+      catchError((e) => {
+        console.error(e);
         this.toastr.error(`
           Attention!! Les données sont fictives, problème avec le serveur.
           Veuillez contacter l'admin.`);
-      },
-    });
-    this.logger.logger("je suis le cvComponent");
-    this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+        return of(this.cvService.getFakeCvs());
+      })
+    );
+    this.selectedCv$ = this.cvService.selectCv$;
+
+    this.logger.logger('je suis le cvComponent');
+    this.toastr.info('Bienvenu dans notre CvTech');
   }
 }
