f86dfdb26f4e18eeb7641de366edbba35f7c8459
iyed <73111142+Iyed73@users.noreply.github.com>
2025-10-24 00:48:24 +0100

    feat: implement todo with signals

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,16 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { Todo } from '../model/todo';
+import { Todo, TodoStatus } from '../model/todo';
 import { TodoService } from '../service/todo.service';
-
 import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-todo',
-    templateUrl: './todo.component.html',
-    styleUrls: ['./todo.component.css'],
-    providers: [TodoService],
-    standalone: true,
-    imports: [FormsModule],
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  standalone: true,
+  imports: [FormsModule],
 })
 export class TodoComponent {
   private todoService = inject(TodoService);
@@ -20,9 +18,8 @@ export class TodoComponent {
 
   /** Inserted by Angular inject() migration for backwards compatibility */
   constructor(...args: unknown[]);
-  constructor() {
-    this.todos = this.todoService.getTodos();
-  }
+  constructor() {}
+
   addTodo() {
     this.todoService.addTodo(this.todo);
     this.todo = new Todo();
@@ -30,5 +27,13 @@ export class TodoComponent {
 
   deleteTodo(todo: Todo) {
     this.todoService.deleteTodo(todo);
+  }
+
+  changeStatus(todo: Todo, newStatus: TodoStatus) {
+    this.todoService.updateTodoStatus(todo, newStatus);
+  }
+
+  getByStatus(status: TodoStatus) {
+    return this.todoService.getByStatus(status);
   }
 }
