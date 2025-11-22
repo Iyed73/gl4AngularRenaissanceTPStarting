6daf78f175b68b2996a727df65e9f9a00048c3c9
loueysioua <sioua.louey@gmail.com>
2025-11-22 23:00:44 +0100

    feat: added form save

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,28 +1,28 @@
-import { Component } from "@angular/core";
-import {
-  AbstractControl,
-  FormBuilder,
-  Validators,
-} from "@angular/forms";
-import { CvService } from "../services/cv.service";
-import { Router } from "@angular/router";
-import { ToastrService } from "ngx-toastr";
-import { APP_ROUTES } from "src/config/routes.config";
-import { Cv } from "../model/cv";
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { CvService } from '../services/cv.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { APP_ROUTES } from 'src/config/routes.config';
+import { Cv } from '../model/cv';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
-  selector: "app-add-cv",
-  templateUrl: "./add-cv.component.html",
-  styleUrls: ["./add-cv.component.css"],
+  selector: 'app-add-cv',
+  templateUrl: './add-cv.component.html',
+  styleUrls: ['./add-cv.component.css'],
 })
 export class AddCvComponent {
+  private formStorageKey = 'cv_draft';
   constructor(
     private cvService: CvService,
     private router: Router,
     private toastr: ToastrService,
     private formBuilder: FormBuilder
   ) {
-    this.age.valueChanges.subscribe((age) => {
+    this.loadDraft();
+
+    this.age.valueChanges.pipe(debounceTime(500)).subscribe((age) => {
       if (age < 18) {
         this.path?.patchValue('');
         this.path?.disable();
@@ -30,28 +30,32 @@ export class AddCvComponent {
         this.path?.enable();
       }
     });
+
+    this.form.valueChanges
+      .pipe(debounceTime(2000), distinctUntilChanged())
+      .subscribe(() => {
+        this.saveDraft();
+      });
   }
 
-  form = this.formBuilder.group(
-    {
-      name: ["", Validators.required],
-      firstname: ["", Validators.required],
-      path: [""],
-      job: ["", Validators.required],
-      cin: [
-        "",
-        {
-          validators: [Validators.required, Validators.pattern("[0-9]{8}")],
-        },
-      ],
-      age: [
-        0,
-        {
-          validators: [Validators.required],
-        },
-      ],
-    },
-  );
+  form = this.formBuilder.group({
+    name: ['', Validators.required],
+    firstname: ['', Validators.required],
+    path: [''],
+    job: ['', Validators.required],
+    cin: [
+      '',
+      {
+        validators: [Validators.required, Validators.pattern('[0-9]{8}')],
+      },
+    ],
+    age: [
+      0,
+      {
+        validators: [Validators.required],
+      },
+    ],
+  });
 
   addCv() {
     this.cvService.addCv(this.form.value as Cv).subscribe({
@@ -68,21 +72,36 @@ export class AddCvComponent {
   }
 
   get name(): AbstractControl {
-    return this.form.get("name")!;
+    return this.form.get('name')!;
   }
   get firstname() {
-    return this.form.get("firstname");
+    return this.form.get('firstname');
   }
   get age(): AbstractControl {
-    return this.form.get("age")!;
+    return this.form.get('age')!;
   }
   get job() {
-    return this.form.get("job");
+    return this.form.get('job');
   }
   get path() {
-    return this.form.get("path");
+    return this.form.get('path');
   }
   get cin(): AbstractControl {
-    return this.form.get("cin")!;
+    return this.form.get('cin')!;
+  }
+
+  saveDraft() {
+    console.log('saveDraft');
+    localStorage.setItem(this.formStorageKey, JSON.stringify(this.form.value));
+  }
+
+  loadDraft() {
+    console.log('loadDraft');
+    const savedDraft = localStorage.getItem(this.formStorageKey);
+    if (savedDraft) {
+      const parsedDraft = JSON.parse(savedDraft) as Cv;
+      this.form.patchValue(parsedDraft);
+      this.form.markAsDirty();
+    }
   }
 }
