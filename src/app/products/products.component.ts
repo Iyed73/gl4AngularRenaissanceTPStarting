0b85aefbb158f65f2d48f6c99299b125a5cad001
maram-br <maram.benrhouma@insat.ucar.tn>
2025-11-23 09:52:54 +0100

    signal

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,29 +1,60 @@
-import { Component } from "@angular/core";
-import {
-  BehaviorSubject,
-  Observable,
-  concatMap,
-  map,
-  takeWhile,
-  scan,
-} from "rxjs";
-import { Product } from "./dto/product.dto";
-import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
-import { AsyncPipe } from "@angular/common";
+import { Component, computed, effect, signal} from '@angular/core';
+import { resource } from '@angular/core/primitives';
+
+import { ProductService } from './services/product.service';
+import { Product } from './dto/product.dto';
+import { Settings } from './dto/product-settings.dto';
 
 @Component({
-    selector: "app-products",
-    templateUrl: "./products.component.html",
-    styleUrls: ["./products.component.css"],
-    standalone: true,
-    imports: [
-    AsyncPipe
-],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-  /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
-  /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
-  constructor() {}
+
+  settings = signal<Settings>({ limit: 12, skip: 0 });
+
+  isLoading = signal(false);
+  noMoreProducts = signal(false);
+
+  allProducts = signal<Product[]>([]);
+
+  constructor(private productService: ProductService) {}
+
+  productsResource = resource({
+    request: () => {
+      this.isLoading.set(true);
+      return this.settings();
+    },
+
+    loader: ({ request }) =>
+      this.productService.getProducts(request),
+  });
+
+  productsEffect = effect(() => {
+    const res = this.productsResource.value();
+
+    if (!res) return;
+
+    if (res.products.length === 0) {
+      this.noMoreProducts.set(true);
+      this.isLoading.set(false);
+      return;
+    }
+
+    this.allProducts.update(p => [...p, ...res.products]);
+
+    this.isLoading.set(false);
+  });
+
+  loadMoreProducts() {
+    if (this.noMoreProducts()) return;
+
+    const { limit, skip } = this.settings();
+
+    this.settings.set({
+      limit,
+      skip: skip + limit
+    });
+  }
 }
