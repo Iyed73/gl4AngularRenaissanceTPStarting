b6d14fb5314296cece692c44bc7fa465e18e256a
loueysioua <sioua.louey@gmail.com>
2025-12-03 00:21:20 +0100

    fix: replaced resource with rxResource

--- src/app/cv/details-cv-signal/details-cv-signal.component.ts
+++ src/app/cv/details-cv-signal/details-cv-signal.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit, inject, resource, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject, signal } from '@angular/core';
 import { Cv } from '../model/cv';
 import { CvService } from '../services/cv.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { APP_ROUTES } from '../../../config/routes.config';
 import { AuthService } from '../../auth/services/auth.service';
 import { DefaultImagePipe } from '../pipes/default-image.pipe';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { catchError, EMPTY, firstValueFrom, tap, throwError } from 'rxjs';
+import { of, tap } from 'rxjs';
 
 @Component({
-    selector: 'app-details-cv-signal',
-    templateUrl: './details-cv-signal.component.html',
-    styleUrls: ['./details-cv-signal.component.css'],
-    standalone: true,
-    imports: [DefaultImagePipe],
+  selector: 'app-details-cv-signal',
+  templateUrl: './details-cv-signal.component.html',
+  styleUrls: ['./details-cv-signal.component.css'],
+  standalone: true,
+  imports: [DefaultImagePipe],
 })
 export class DetailsCvSignalComponent {
   private cvService = inject(CvService);
@@ -26,34 +25,35 @@ export class DetailsCvSignalComponent {
   private activatedRoute = inject(ActivatedRoute);
   private id = signal(+this.activatedRoute.snapshot.params['id']);
 
-   cvResource = resource<Cv | null, number>({
+  cvResource = rxResource<Cv | null, number>({
     params: () => this.id(),
-    loader: async ({ params: cvId }) => {
-      const cvData = await firstValueFrom(this.cvService.getCvById(cvId));
-      this.cv.set(cvData);
-      return cvData;
-    },
-    defaultValue: null
-  });
-
- cvToDelete = signal<Cv | null>(null);
-  deleteResource = resource<void, Cv | null>({
-    params: () => this.cvToDelete(),
-    loader: async ({ params: cv }) => {
-      if (!cv) return;
-      await firstValueFrom(
-        this.cvService.deleteCvById(cv.id).pipe(
-          tap(() => {
-            this.router.navigate(['/cv']);
-          })
-        )
+    stream: ({ params: cvId }) => {
+      return this.cvService.getCvById(cvId).pipe(
+        tap((cvData) => {
+          this.cv.set(cvData);
+        })
       );
     },
-    defaultValue: undefined
+    defaultValue: null,
+  });
+
+  cvToDelete = signal<Cv | null>(null);
+  deleteResource = rxResource<null, Cv | null>({
+    params: () => this.cvToDelete(),
+    stream: ({ params: cv }) => {
+      if (!cv) return of(null);
+
+      return this.cvService.deleteCvById(cv.id).pipe(
+        tap(() => {
+          this.router.navigate(['/cv']);
+        })
+      );
+    },
+    defaultValue: undefined,
   });
 
   deleteCv(cv: Cv) {
-    this.cvToDelete.set(cv);    
-    this.deleteResource.reload(); 
+    this.cvToDelete.set(cv);
+    this.deleteResource.reload();
   }
 }
